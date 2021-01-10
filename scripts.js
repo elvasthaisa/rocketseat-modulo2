95100f0e976c05014b286d5bfa75617754728d28
Thaísa Elvas <thaisaelvas@gmail.com>
2021-01-10 17:32:48 -0300

    controlling the modal with javascript

---
+++ scripts.js
@@ -0,0 +1,11 @@
+const modalOverlay = document.querySelector('.modal-overlay');
+const cards = document.querySelectorAll('.card');
+
+for (let card of cards) {
+    card.addEventListener("click", () => modalOverlay.classList.add("active"))
+}
+
+document.querySelector('.close-modal').addEventListener("click", () => {
+    modalOverlay.classList.remove("active");
+    console.log('you clicked me!');
+})
